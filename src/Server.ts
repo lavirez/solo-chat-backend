f78bdeed83c1546b09cfbb8d5bc5693f88b5e4ec
alireza-a-razavi <alirezaalavirazavi@hotmail.com>
2023-01-02 11:23:53 +0330

    added new dependancies

--- src/Server.ts
+++ src/Server.ts
@@ -1,7 +1,8 @@
 import cookieParser from "cookie-parser"; 
-import express from "experss"; 
+import express from "express"; 
+import { Request, Response, NextFunction } from "express";
 import { BAD_REQUEST } from "http-status-codes"; 
-import BaseRouter from ",/routes";
+import BaseRouter from "./routes";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,12 @@ app.use(express.urlencoded({extened: true}));
 app.use(cookieParser());
 
 app.use('/api', BaseRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(err.message, err);
+    return res.status(BAD_REQUEST.json({
+        error: err.message,
+    }));
+});
 
 export default app; 

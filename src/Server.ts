916b8ad98f56a97165dc85541595d3277a69f99d
alireza-a-razavi <alirezaalavirazavi@hotmail.com>
2023-01-03 23:38:01 +0330

    added nodemon for ts

--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser"; 
 import express from "express"; 
+import cors from "cors"; 
 import { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import { BAD_REQUEST } from "http-status-codes"; 
@@ -8,6 +9,7 @@ import BaseRouter from "./routes";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import app from "./Server";
-import { initializeDB , initializeCache } from './db';
+// import { initializeDB , initializeCache } from './db';
 
-const REDIS_PORT = Number(process.env.REDIS_PORT || 6379);
+// const REDIS_PORT = Number(process.env.REDIS_PORT || 6379);
 const PORT = Number(process.env.API_PORT || process.env.PORT || 5002);
 
-initializeDB();
-initializeCache(REDIS_PORT);
+// initializeDB();
+// initializeCache(REDIS_PORT);
 
 app.listen(PORT, () => {
     console.log("Express started on port: " + PORT); 
